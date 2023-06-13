712f5538146a24c639ad840cec6c1e569b623fad
kks007 <kaushalkishor10702@gmail.com>
2023-06-13 22:44:39 +0530

    .

--- 121_best_time_to_sell_stock.cpp
+++ 121_best_time_to_sell_stock.cpp
@@ -2,27 +2,44 @@
 
 using namespace std;
 
+
+
+//// optimised method : two pointer::
+
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int least_so_far = INT_MAX;
-        int overall_profit = 0;
-        int profit_temp = 0;
-
-        for(int i = 0; i < prices.size(); i++){
-            if(prices[i] < least_so_far){
-                least_so_far = prices[i];
-            }
-            profit_temp = prices[i] - least_so_far;
-            if(overall_profit < profit_temp){
-                overall_profit = profit_temp;
-            }
+        int maxP = 0, l = 0, r = 0;
+        while (r < prices.size()){
+            if (prices[r] > prices[l])
+                maxP = max(maxP, prices[r] - prices[l]);
+            else
+                l = r;
+            ++r;
         }
-        return overall_profit;
-        
+        return maxP;
     }
 };
 
-int main(){
-    
-}
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int least_so_far = INT_MAX;
+//         int overall_profit = 0;
+//         int profit_temp = 0;
+
+//         for(int i = 0; i < prices.size(); i++){
+//             if(prices[i] < least_so_far){
+//                 least_so_far = prices[i];
+//             }
+//             profit_temp = prices[i] - least_so_far;
+//             if(overall_profit < profit_temp){
+//                 overall_profit = profit_temp;
+//             }
+//         }
+//         return overall_profit;
+        
+//     }
+// };
+
