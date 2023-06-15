e2ba807a12ab90fb31818d04a650c9f4098b04d0
kks007 <kaushalkishor10702@gmail.com>
2023-06-15 21:08:15 +0530

    '

---
+++ 3_longest_substring_wihout_repeated_characters.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string& s) {
+        unordered_set<char> chars;
+        int maxSize = 0;
+        int i = 0, j = 0;
+        while (j < s.size()){
+            while (chars.find(s[j]) != chars.end()){
+                chars.erase(s[i]);
+                ++i;
+            }
+            maxSize = max(maxSize, j - i + 1);
+            chars.insert(s[j]);
+            ++j;
+        }
+        return maxSize;
+    }
+};
