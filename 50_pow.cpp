43688b4b4711418b17caa7a06355be91a9075866
Kaushal Kishor Sharma <kaushalkishor10702@gmail.com>
2023-06-23 18:32:03 +0530

    Create 50_pow.cpp

---
+++ 50_pow.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long exponent = abs(n);
+        double curr = x;
+        double result = 1.0;
+        
+        for (long i = exponent; i > 0; i /= 2) {
+            if (i % 2 == 1) {
+                result *= curr;
+            }
+            curr *= curr;
+        }
+        
+        if (n < 0) {
+            return 1.0 / result;
+        }
+        return result;
+    }
+};
